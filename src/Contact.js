726222a58220c90085737af895e756ad37b32e85
RajasG <rajasmgandhi@gmail.com>
2020-04-29 18:22:39 -0500

    another update

--- src/Contact.js
+++ src/Contact.js
@@ -3,30 +3,30 @@ import React from 'react';
 function Contact() {
     return(
         <div>
-        <form name="contactform" id="contactform" enctype="text/plain" action="" >
+        <form name="contactform" id="contactform" action="https://hooks.zapier.com/hooks/catch/7397875/or2336l/" type="POST">
     <p>
       <label>First Name:<br />
-        <input name="entry.228158447" type="text" id="entry.22815847" size="30" />
+        <input name="firstname" type="text" id="entry.22815847" size="30" />
       </label>
     </p>
     <p>
       <label>Last Name:<br />
-        <input name="entry.2098725634" type="text" id="entry.2098725634" size="30" />
+        <input name="lastname" type="text" id="entry.2098725634" size="30" />
       </label>
     </p>
     <p>
       <label>Email:<br />
-        <input name="entry.546969202" type="text" id="entry.546969202" size="48" />
+        <input name="email" type="text" id="entry.546969202" size="48" />
       </label>
     </p>
     <p>
       <label>Message:<br />
-        <textarea name="entry.82328721" id="entry.82328721" cols="50" rows="7"></textarea>
+        <textarea name="message" id="entry.82328721" cols="50" rows="7"></textarea>
       </label>
     </p>
     <p>
       <label>
-        <input name="button" type="submit" class="submit-button" id="button" value="Send" />
+        <input name="button" type="submit" class="submit-button" id="form-submit" value="Submit" />
       </label>
     </p>
   </form>
