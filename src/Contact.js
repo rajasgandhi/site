0e8a5c5b0928d6255fdb1130644d5c6e79129be0
RajasG <rajasmgandhi@gmail.com>
2020-05-02 14:22:51 -0500

    try to get it working

--- src/Contact.js
+++ src/Contact.js
@@ -1,37 +1,39 @@
 import React from 'react';
 
 function Contact() {
-    return(
-        <div>
+  return (
+    <div>
+      <div id="wrapper">
         <form name="contactform" id="contactform">
-    <p>
-      <label>First Name:<br />
-        <input name="firstname" type="text" id="entry.22815847" size="30" required/>
-      </label>
-    </p>
-    <p>
-      <label>Last Name:<br />
-        <input name="lastname" type="text" id="entry.2098725634" size="30" required/>
-      </label>
-    </p>
-    <p>
-      <label>Email:<br />
-        <input name="email" type="email" id="entry.546969202" size="48" required/>
-      </label>
-    </p>
-    <p>
-      <label>Message:<br />
-        <textarea name="message" id="entry.82328721" cols="50" rows="7" required></textarea>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="button" type="submit" className="submit-button" id="form-submit" value="Submit" />
-      </label>
-    </p>
-  </form>
+          <p>
+            <label>First Name:<br />
+              <input name="firstname" type="text" id="entry.22815847" size="30" required />
+            </label>
+          </p>
+          <p>
+            <label>Last Name:<br />
+              <input name="lastname" type="text" id="entry.2098725634" size="30" required />
+            </label>
+          </p>
+          <p>
+            <label>Email:<br />
+              <input name="email" type="email" id="entry.546969202" size="48" required />
+            </label>
+          </p>
+          <p>
+            <label>Message:<br />
+              <textarea name="message" id="entry.82328721" cols="50" rows="7" required></textarea>
+            </label>
+          </p>
+          <p>
+            <label>
+              <input name="button" type="submit" className="submit-button" id="form-submit" value="Submit" />
+            </label>
+          </p>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Contact;
