57fc7fecebde7fe0f4d50487fac726587acc5cf4
RajasG <rajasmgandhi@gmail.com>
2020-05-02 11:56:04 -0500

    update contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Components/Header';
 
 function Contact() {
     return(
         <div>
+          <Header />
         <form name="contactform" id="contactform">
     <p>
       <label>First Name:<br />
