1bdde52a4d2654a563db9afaa64fcaca18f7346c
rajasgandhi <rajasmgandhi@gmail.com>
2020-06-26 12:30:50 -0500

    update

--- api/contact/index.php
+++ api/contact/index.php
@@ -1,7 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
 
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
