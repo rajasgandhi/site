571073bd2eae2658f0351931c167eb35a80884d4
rajasgandhi <rajasmgandhi@gmail.com>
2020-06-27 18:14:22 -0500

    Update index.php

--- api/contact/index.php
+++ api/contact/index.php
@@ -1,6 +1,7 @@
 <?php
-header("access-control-allow-origin: *");
-header("access-control-allow-headers: content-type");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
